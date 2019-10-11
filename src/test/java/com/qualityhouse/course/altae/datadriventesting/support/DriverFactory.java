b1f89ea94a6401ac7c1ea83bd2e55a40691f948b
Yakim Rachev <jakimcho@gmail.com>
2019-10-11 23:12:09 +0300

    fixed site url and updated driver factory to set up chrome driver path

--- src/test/java/com/qualityhouse/course/altae/datadriventesting/support/DriverFactory.java
+++ src/test/java/com/qualityhouse/course/altae/datadriventesting/support/DriverFactory.java
@@ -4,24 +4,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DriverFactory
 {
     public static WebDriver getDriver( String browser )
     {
+        setDriversPaths();
         WebDriver driver = null;
-        switch( browser )
+        switch ( browser )
         {
             case "Chrome":
-                driver = new ChromeDriver( );
+                driver = new ChromeDriver();
                 break;
             case "Firefox":
-                driver = new FirefoxDriver( );
+                driver = new FirefoxDriver();
                 break;
             default:
-                driver = new ChromeDriver( );
+                driver = new ChromeDriver();
 
         }
 
         return driver;
+    }
+
+    private static void setDriversPaths()
+    {
+        Map<String, String> driversPath = new HashMap<>();
+        driversPath.put( "linux",
+                         "drivers/linux/chromedriver" );
+        driversPath.put( "mac",
+                         "drivers/mac/chromedriver" );
+        driversPath.put( "windows",
+                         "drivers/windows/chromedriver.exe" );
+        String tasOS = System.getProperty( "os.name" )
+                             .toLowerCase()
+                             .split( " " )[0];
+        System.out.println( "Current OS is: " + tasOS );
+        System.setProperty( "webdriver.chrome.driver",
+                            driversPath.get( tasOS ) );
     }
 }
